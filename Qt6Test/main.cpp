cb9e57e5b9c96eae7d09a567c295f6dcbcc31c28
BH2WFR <vb60qiyebanzyy@163.com>
2023-11-12 18:52:56 +0800

    我也不知道改了啥

--- Qt6Test/main.cpp
+++ Qt6Test/main.cpp
@@ -1,19 +1,37 @@
 #include "main.h"
 #include "mainwindow.h"
 
-
+#include <qglobal.h>
+#include <QObject>
 #include <QApplication>
 #include <TerminalBeauty.h>
 
+#include <QDebug>
+
+
+
+
+
+
+
 int main(int argc, char *argv[])
 {
-    //QT_HIGH_DPI();
+QT_HIGH_DPI();
     //QApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
     QApplication a(argc, argv);
     MainWindow w;
     
     //* 调试命令行
 	QDEBUGNN << "=================== Project Debugging  中文 ================";
+    
+    A aa;
+    aa.MySignal();
+    
+    std::unique_ptr<A> b1 {std::make_unique<B1>()};
+    b1->MySignal();
+    
+    std::shared_ptr<A> b2 {std::make_shared<B2>()};
+    emit b2->MySignal();
     
     w.show();
     return a.exec();
--- Qt6Test/main.h
+++ Qt6Test/main.h
@@ -3,6 +3,57 @@
 
 #ifdef __cplusplus
 
+#include <qglobal.h>
+#include <QObject>
+#include <QApplication>
+#include <TerminalBeauty.h>
+
+#include <QDebug>
+
+
+class A : public QObject{
+    Q_OBJECT
+public:
+    QString shabi {"shabi"};
+
+signals:
+    void MySignal();
+    
+public slots:
+    virtual void MySlot(){
+        qDebug() << "A";
+    }
+public:
+    A(){
+        //QObject::connect(pushButtonCntUp, SIGNAL(clicked()), MainWindow, SLOT(btnCntUp_Click()));
+        QObject::connect(this, SIGNAL(MySignal()), this, SLOT(MySlot()));
+    }
+};
+
+struct B1 : public A{
+    Q_OBJECT
+public:
+    B1() : A() {
+        shabi = "B1";
+    }
+public slots:
+	virtual void MySlot() override{
+		qDebug() << "B1";
+	}
+};
+
+struct B2 : public A{
+    Q_OBJECT
+public:
+    B2() : A() {
+        shabi = "B2";
+    }
+public slots:
+	virtual void MySlot() override{
+		qDebug() << "B2";
+	}
+};
+
 #endif // __cplusplus
 
 #endif //MAIN_H__
